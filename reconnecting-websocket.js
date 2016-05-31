c55c93404ed81b945d6e6da63043a5d05b8c2207
Paul Mennega <paul@bitgold.com>
2016-05-31 16:20:15 -0400

    Update to connection logic, return object structure

--- reconnecting-websocket.js
+++ reconnecting-websocket.js
@@ -120,27 +120,32 @@
   function ReconnectingWebSocket(url, protocols, options) {
     // Default settings
     var settings = {
-      /** Whether this instance should log debug messages. */
-      debug: false,
-
       /** Whether or not the websocket should attempt to connect immediately upon instantiation. */
       automaticOpen: true,
 
-      /** The number of milliseconds to delay before attempting to reconnect. */
-      reconnectInterval: 1000,
-      /** The maximum number of milliseconds to delay a reconnection attempt. */
-      maxReconnectInterval: 30000,
-      /** The rate of increase of the reconnect delay. Allows reconnect attempts to back off when problems persist. */
-      reconnectDecay: 1.5,
+      /** The binary type, possible values 'blob' or 'arraybuffer', default 'blob'. */
+      binaryType: 'blob',
 
-      /** The maximum time in milliseconds to wait for a connection to succeed before closing and retrying. */
-      timeoutInterval: 2000,
+      /** Whether this instance should log debug messages. */
+      debug: false,
 
       /** The maximum number of reconnection attempts to make. Unlimited if null. */
       maxReconnectAttempts: null,
 
-      /** The binary type, possible values 'blob' or 'arraybuffer', default 'blob'. */
-      binaryType: 'blob'
+      /** The maximum number of milliseconds to delay a reconnection attempt. */
+      maxReconnectInterval: 30000,
+
+      /** SHould we add a random (small) value to the reconnection timing so that we can hep space out connection calls? */
+      randomizeReconnect: false,
+
+      /** The rate of increase of the reconnect delay. Allows reconnect attempts to back off when problems persist. */
+      reconnectDecay: 1.5,
+
+      /** The number of milliseconds to delay before attempting to reconnect. */
+      reconnectInterval: 1000,
+
+      /** The maximum time in milliseconds to wait for a connection to succeed before closing and retrying. */
+      timeoutInterval: 2000
     }
     if (!options) {
       options = {};
@@ -231,7 +236,7 @@
         if (self.debug || ReconnectingWebSocket.debugAll) {
           console.debug('ReconnectingWebSocket', 'connection-timeout', self.url);
         }
-        
+
         timedOut = true;
         localWs.close();
         timedOut = false;
@@ -293,11 +298,11 @@
             }));
           }
 
-          var timeout = self.reconnectInterval * Math.pow(self.reconnectDecay, self.reconnectAttempts);
-          setTimeout(function() {
+          var timeout = (self.reconnectInterval + ((self.randomizeReconnect) ? Math.random() : 0)) * Math.pow(self.reconnectDecay, self.reconnectAttempts);
+          setTimeout(function () {
             self.reconnectAttempts++;
             self.open(true);
-          }, timeout > self.maxReconnectInterval ? self.maxReconnectInterval : timeout);
+          }, (timeout > self.maxReconnectInterval) ? self.maxReconnectInterval : timeout);
         }
       };
       ws.onmessage = function (event) {
